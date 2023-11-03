75acbe88d45da13edaa9d7cc9e7c6277ad448ae1
clien9025 <302677599@qq.com>
2023-11-03 09:01:13 +0800

    基本的有参,无参方法

---
+++ src/main/java/zhanyang/ExploreArrayList.java
@@ -0,0 +1,97 @@
+package zhanyang;
+
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.function.IntFunction;
+import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
+import java.util.stream.Stream;
+
+public class ExploreArrayList<E> extends AbstractList<E>
+        implements List<E>, RandomAccess, Cloneable, java.io.Serializable {
+
+    transient Object[] elementData;
+    private static final Object[] EMPTY_ELEMENTDATA = {};
+    private static final Object[] DEFAULTCAPACITY_EMPTY_ELEMENTDATA = {};
+    private int size;
+
+    // 判断 数组的容量
+    public ExploreArrayList(int initialCapacity) {
+        if (initialCapacity > 0) {
+            this.elementData = new Object[initialCapacity];
+        } else if (initialCapacity == 0) {
+            this.elementData = EMPTY_ELEMENTDATA;
+        } else {
+            throw new IllegalArgumentException("Illegal capacity:" + initialCapacity);
+        }
+    }
+
+    public ExploreArrayList() {
+        this.elementData = DEFAULTCAPACITY_EMPTY_ELEMENTDATA;
+    }
+
+    public ExploreArrayList(Collection<? extends E> c) {
+        Object[] a = c.toArray();
+        if ((size = a.length) != 0) {
+            if (c.getClass() == ArrayList.class) {
+                elementData = a;
+            } else {
+                elementData = Arrays.copyOf(a, size, Object[].class);
+            }
+        } else {
+            elementData = DEFAULTCAPACITY_EMPTY_ELEMENTDATA;
+        }
+    }
+
+
+    @Override
+
+    public E get(int index) {
+        return null;
+    }
+
+    @Override
+    public void forEach(Consumer<? super E> action) {
+        super.forEach(action);
+    }
+
+    @Override
+    public Spliterator<E> spliterator() {
+        return List.super.spliterator();
+    }
+
+    @Override
+    public Stream<E> stream() {
+        return super.stream();
+    }
+
+    @Override
+    public Stream<E> parallelStream() {
+        return super.parallelStream();
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public <T> T[] toArray(IntFunction<T[]> generator) {
+        return super.toArray(generator);
+    }
+
+    @Override
+    public boolean removeIf(Predicate<? super E> filter) {
+        return super.removeIf(filter);
+    }
+
+    @Override
+    public void replaceAll(UnaryOperator<E> operator) {
+        List.super.replaceAll(operator);
+    }
+
+    @Override
+    public void sort(Comparator<? super E> c) {
+        List.super.sort(c);
+    }
+}
